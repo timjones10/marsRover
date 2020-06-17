a3c761fedffd1ea00984ddd6b62ccea072f5f1c3
UK80131232 <tim.jones2@tesco.com>
2020-06-17 14:04:05 +0100

    Adds N and S facing tests

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,12 +1,61 @@
 public class MarsRover {
 
+    private int xAxis;
+    private int yAxis;
+    private String facingDirection;
+
     public MarsRover(int xAxis, int yAxis, String facingDirection) {
+        this.xAxis = xAxis;
+        this.yAxis = yAxis;
+        this.facingDirection = facingDirection;
     }
 
     public int[] getPosition() {
-        return new int[]{0, 1};
+        return new int[]{xAxis, yAxis};
     }
 
-    public void moveForward() {
+    public void move(char[] directions) {
+        if ("N".equals(facingDirection)) {
+            handleNorthFacingRoverMovement(directions[0]);
+        }
+        if ("S".equals(facingDirection)) {
+            handleSouthFacingRoverMovement(directions[0]);
+        }
+        if ("E".equals(facingDirection)) {
+            if (directions[0] == 'F') {
+                xAxis++;
+            }
+        }
+    }
+
+
+    private void handleNorthFacingRoverMovement(char direction) {
+        if (direction == 'F') {
+            yAxis++;
+        }
+        if (direction == 'B') {
+            yAxis--;
+        }
+        if (direction == 'L') {
+            xAxis--;
+        }
+        if (direction == 'R') {
+            xAxis++;
+        }
+    }
+
+    private void handleSouthFacingRoverMovement(char direction) {
+        if (direction == 'F') {
+            yAxis--;
+        }
+        if (direction == 'B') {
+            yAxis++;
+        }
+        if (direction == 'L') {
+            xAxis++;
+        }
+        if (direction == 'R') {
+            xAxis--;
+        }
     }
 }
--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -5,11 +5,83 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MarsRoverTest {
 
     @Test
-    void roverMovesForwardWhenItReceivesFCommand() {
+    void roverFacingNorthReceivesMoveForwardCommand() {
         MarsRover rover = new MarsRover(0, 0, "N");
 
-        rover.moveForward();
+        rover.move(new char[]{'F'});
 
         assertThat(rover.getPosition()).isEqualTo(new int[]{0, 1});
+    }
+
+    @Test
+    void roverFacingNorthReceivesMoveBackwardCommand() {
+        MarsRover rover = new MarsRover(0, 0, "N");
+
+        rover.move(new char[]{'B'});
+
+        assertThat(rover.getPosition()).isEqualTo(new int[]{0, -1});
+    }
+
+    @Test
+    void roverFacingNorthReceivesMoveLeftCommand() {
+        MarsRover rover = new MarsRover(0, 0, "N");
+
+        rover.move(new char[]{'L'});
+
+        assertThat(rover.getPosition()).isEqualTo(new int[]{-1, 0});
+    }
+
+    @Test
+    void roverFacingNorthReceivesMoveRightCommand() {
+        MarsRover rover = new MarsRover(0, 0, "N");
+
+        rover.move(new char[]{'R'});
+
+        assertThat(rover.getPosition()).isEqualTo(new int[]{1, 0});
+    }
+
+    @Test
+    void roverFacingSouthReceivesMoveForwardCommand() {
+        MarsRover rover = new MarsRover(0, 0, "S");
+
+        rover.move(new char[]{'F'});
+
+        assertThat(rover.getPosition()).isEqualTo(new int[]{0, -1});
+    }
+
+    @Test
+    void roverFacingSouthReceivesMoveBackwardsCommand() {
+        MarsRover rover = new MarsRover(0, 0, "S");
+
+        rover.move(new char[]{'B'});
+
+        assertThat(rover.getPosition()).isEqualTo(new int[]{0, 1});
+    }
+
+    @Test
+    void roverFacingSouthReceivesMoveLeftCommand() {
+        MarsRover rover = new MarsRover(0, 0, "S");
+
+        rover.move(new char[]{'L'});
+
+        assertThat(rover.getPosition()).isEqualTo(new int[]{1,0});
+    }
+
+    @Test
+    void roverFacingSouthReceivesMoveRightCommand(){
+        MarsRover rover = new MarsRover(0,0,"S");
+
+        rover.move(new char[]{'R'});
+
+        assertThat(rover.getPosition()).isEqualTo(new int[]{-1, 0});
+    }
+
+    @Test
+    void roverFacingEastReceivesMoveForwardCommand(){
+        MarsRover rover = new MarsRover(0,0,"E");
+
+        rover.move(new char[]{'F'});
+
+        assertThat(rover.getPosition()).isEqualTo(new int[]{1,0});
     }
 }
